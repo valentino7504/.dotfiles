2e2b823c24020e999ab519920d81450a7decaf71
Edwin Ade <edwinmbonyjr@gmail.com>
2025-03-29 08:03:58 +0100

    feat(core): move TextYankPost to init.lua

--- .config/nvim/lua/valentino7504/core/init.lua
+++ .config/nvim/lua/valentino7504/core/init.lua
@@ -1,2 +1,21 @@
 require("valentino7504.core.options")
 require("valentino7504.core.keymaps")
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.{js,c,ts,go,jsx,css,py}",
+	callback = function()
+		local save_cursor = vim.fn.getpos(".")
+		vim.cmd([[%s/\s\+$//e]])
+		vim.fn.setpos(".", save_cursor)
+	end,
+})
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual" })
+	end,
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+	callback = function()
+		vim.cmd("setlocal nonumber norelativenumber")
+	end,
+})
