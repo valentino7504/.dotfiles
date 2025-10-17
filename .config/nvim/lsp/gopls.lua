9640cb2fbe11b29ae06142097fbb06cbcc4faba6
Edwin Ade <edwinmbonyjr@gmail.com>
2025-10-17 13:35:41 +0100

    feat(gopls): update filetypes list

--- .config/nvim/lsp/gopls.lua
+++ .config/nvim/lsp/gopls.lua
@@ -1,5 +1,5 @@
 vim.lsp.config("gopls", {
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	filetypes = { "go", "go.mod", "go.work", "gotmpl" },
 	settings = {
 		gopls = {
 			completeFunctionCalls = false,
