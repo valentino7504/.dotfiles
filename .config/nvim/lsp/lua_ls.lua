5b35f9c9298a8d0140a2a75b20b52583dbfc3032
Edwin Ade <edwinmbonyjr@gmail.com>
2026-02-17 04:08:48 +0000

    fix: issue with workspaces in lua 3.17 resolved for now

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -1,6 +1,16 @@
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
+			-- this is because of the lua 3.17.0 update
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					{ path = "snacks.nvim", words = { "Snacks" } }, -- others you need to load
+				},
+			},
+			-- ends here
 			runtime = { version = "LuaJIT" },
 			completion = { callSnippet = "Replace" },
 			telemetry = { enable = false },
