1fbe327a0e7eb5cff79ffba8812ea5fe95dae2cd
Edwin Ade <edwinmbonyjr@gmail.com>
2026-02-21 18:01:03 +0000

    feat: utilise new api for get option value

--- nvim/.config/nvim/lsp/ts_ls.lua
+++ nvim/.config/nvim/lsp/ts_ls.lua
@@ -8,7 +8,7 @@ vim.lsp.config("ts_ls", {
 	handlers = {
 		["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
 			local uri = result.uri
-			local filetype = vim.api.nvim_buf_get_option_value(vim.uri_to_bufnr(uri), "filetype")
+			local filetype = vim.api.nvim_get_option_value("filetype", { buf = vim.uri_to_bufnr(uri) })
 			-- Only process diagnostics for TypeScript files
 			if filetype == "typescript" or filetype == "typescriptreact" then
 				vim.lsp.handlers["textDocument/publishDiagnostics"](_, result, ctx, config)
