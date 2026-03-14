29fecbc009a6dfb4a9f51e1098478fd70c6a7761
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-14 16:19:23 +0000

    fix: lazy load mason to improve startup times

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,13 +1,12 @@
 return {
 	{
 		"mason-org/mason.nvim",
+		cmd = "Mason",
 		dependencies = {
-			"mason-org/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
 		config = function()
 			local mason = require("mason")
-			local mason_lspconfig = require("mason-lspconfig")
 			local mason_tool_installer = require("mason-tool-installer")
 
 			mason.setup({
@@ -17,23 +16,6 @@ return {
 						package_pending = "➜",
 						package_uninstalled = "✗",
 					},
-				},
-			})
-
-			mason_lspconfig.setup({
-				ensure_installed = {
-					"html",
-					"cssls",
-					"omnisharp",
-					"emmet_ls",
-					"lua_ls",
-					"gopls",
-					-- "jdtls",
-					"ty",
-					"docker_compose_language_service",
-					"dockerls",
-					"ts_ls",
-					"rust_analyzer",
 				},
 			})
 
@@ -50,6 +32,32 @@ return {
 					"gofumpt",
 					"goimports",
 					"hadolint",
+				},
+			})
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
+		config = function()
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"html",
+					"cssls",
+					"omnisharp",
+					"emmet_ls",
+					"lua_ls",
+					"gopls",
+					-- "jdtls",
+					"ty",
+					"docker_compose_language_service",
+					"dockerls",
+					"ts_ls",
+					"rust_analyzer",
 				},
 			})
 		end,
