a7f7a0ca1662d3b4c4422784461d15d312269850
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-16 21:26:22 +0000

    feat: add minuet for completion using gemini

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
+local ok, secrets = pcall(require, "secrets")
+if ok then
+	vim.env.GEMINI_API_KEY = secrets.GEMINI_API_KEY
+end
 require("core")
 require("lazy_setup")
 require("lsp_setup")
---
+++ nvim/.config/nvim/lua/plugins/minuet.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"milanglacier/minuet-ai.nvim",
+		config = function()
+			require("minuet").setup({
+				-- Your configuration options here
+				provider = "gemini",
+				provider_options = {
+					gemini = {
+						model = "gemini-2.5-flash-lite",
+						optional = {
+							generationConfig = {
+								maxOutputTokens = 256,
+								thinkingConfig = {
+									thinkingBudget = 0,
+								},
+							},
+						},
+					},
+				},
+				context_window = 4000,
+				context_ratio = 0.75,
+				throttle = 2000,
+				debounce = 800,
+				virtualtext = {
+					auto_trigger_ft = {},
+					keymap = {
+						accept = "<Tab>",
+					},
+				},
+				request_timeout = 2.5,
+			})
+			vim.keymap.set("n", "<leader>mt", ":Minuet virtualtext enable<CR>", { desc = "Toggle AI completion" })
+		end,
+	},
+	{ "nvim-lua/plenary.nvim" },
+}
