5202eabf27e86485039d04cf054c9e5a385feb40
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-28 16:58:11 +0000

    feat: update statusline

--- nvim/.config/nvim/lua/plugins/mini/statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini/statusline.lua
@@ -2,10 +2,35 @@ return {
 	"nvim-mini/mini.statusline",
 	version = false,
 	config = function()
+		local wakatime_cli = vim.fn.expand("~/.wakatime/wakatime-cli")
+		local wakatime_time = ""
+
+		local function fetch_wakatime()
+			if vim.fn.executable(wakatime_cli) ~= 1 then
+				return
+			end
+			vim.system({ wakatime_cli, "--today" }, { text = true }, function(result)
+				if result.code == 0 and result.stdout then
+					local trimmed = result.stdout:gsub("%s+$", ""):gsub(" hrs?", "h"):gsub(" mins?", "m")
+					vim.schedule(function()
+						wakatime_time = trimmed ~= "" and ("⏱ " .. trimmed) or ""
+					end)
+				end
+			end)
+		end
+
+		fetch_wakatime()
+
+		local timer = vim.uv.new_timer()
+		if not timer then
+			vim.notify("WakaTime: failed to create timer", vim.log.levels.WARN)
+			return
+		end
+		timer:start(5 * 60 * 1000, 5 * 60 * 1000, fetch_wakatime)
+
 		require("mini.statusline").setup({
 			content = {
 				active = function()
-					-- Existing mini.statusline sections
 					local mode, location, git, diag, fileinfo, filename, lsp =
 						MiniStatusline.section_mode({}),
 						MiniStatusline.section_location({}),
@@ -15,18 +40,18 @@ return {
 						MiniStatusline.section_filename({}),
 						MiniStatusline.section_lsp({})
 
-					-- time section: HH:MM
 					local time = os.date("%I:%M%p")
 
 					return MiniStatusline.combine_groups({
 						{ hl = "MiniStatuslineModeNormal", strings = { mode } },
 						{ hl = "MiniStatuslineDevinfo", strings = { lsp, git, diag } },
-						"%<", -- Truncation point
+						"%<",
 						{ hl = "MiniStatuslineFilename", strings = { filename } },
-						"%=", -- Right align
+						"%=",
 						{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
 						{ hl = "MiniStatuslineModeOther", strings = { location } },
-						{ hl = "MiniStatuslineModeNormal", strings = { time } }, -- Time added here
+						{ hl = "MiniStatuslineModeNormal", strings = { wakatime_time } },
+						{ hl = "MiniStatuslineModeNormal", strings = { " " .. time } },
 					})
 				end,
 			},
