17652492601c306248d439a7a7e70a5159a18a1b
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-30 13:15:37 +0100

    feat: remove autoload sessions

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,5 +1,8 @@
+local augroup = vim.api.nvim_create_augroup("user_autocmds", { clear = true })
+
 -- Remove autocmds when in leetcode buffers
 vim.api.nvim_create_autocmd("BufEnter", {
+	group = augroup,
 	callback = function(args)
 		local bufname = vim.api.nvim_buf_get_name(args.buf)
 		if string.find(bufname, "/.local/share/nvim/leetcode/") then
@@ -10,19 +13,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup,
 	callback = function()
 		vim.hl.on_yank({ timeout = 200 })
-	end,
-})
-
--- Auto-restore session if one exists in cwd (only when opening nvim with no args)
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		if vim.fn.argc() == 0 then
-			local session = vim.fn.getcwd() .. "/.session.vim"
-			if vim.fn.filereadable(session) == 1 then
-				vim.cmd("source " .. session)
-			end
-		end
 	end,
 })
