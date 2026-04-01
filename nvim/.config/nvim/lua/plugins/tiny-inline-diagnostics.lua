d352289684b3410bfdd21f1188fd669a81547493
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-01 15:03:48 +0100

    feat: move some plugins setup to opts

--- nvim/.config/nvim/lua/plugins/tiny-inline-diagnostics.lua
+++ nvim/.config/nvim/lua/plugins/tiny-inline-diagnostics.lua
@@ -2,15 +2,13 @@ return {
 	"rachartier/tiny-inline-diagnostic.nvim",
 	event = "LspAttach",
 	priority = 1000,
-	config = function()
-		require("tiny-inline-diagnostic").setup({
-			preset = "amongus",
-			options = {
-				show_code = true,
-				overflow = {
-					mode = "wrap",
-				},
+	opts = {
+		preset = "classic",
+		options = {
+			show_code = true,
+			overflow = {
+				mode = "wrap",
 			},
-		})
-	end,
+		},
+	},
 }
