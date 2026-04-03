ebb1084763f8c3ecbda408d21cd4a7bc71300d75
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-03 17:36:09 +0100

    feat: remove unnecessary settings in nvim-lspconfig

--- nvim/.config/nvim/lua/lsp_setup.lua
+++ nvim/.config/nvim/lua/lsp_setup.lua
@@ -1,27 +1,12 @@
--- Define servers that are allowed to show inlay hints
+-- Define necessary variables - inlay hint allowed servers and keymap
 local hint_allowed_list = {
 	["ty"] = true,
 	["rust_analyzer"] = true,
 	["gopls"] = true,
 }
+local keymap = vim.keymap
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		local bufnr = args.buf
-		-- Check if the current server is in our allowed list
-		if client == nil then
-			return
-		end
-		client.server_capabilities.semanticTokensProvider = nil
-		if hint_allowed_list[client.name] then
-			if client.server_capabilities.inlayHintProvider then
-				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-			end
-		end
-	end,
-})
-
+-- Setup diagnostics
 vim.diagnostic.config({
 	virtual_text = false,
 	virtual_lines = false, -- these two are off because of tiny inline diagnostics
@@ -40,4 +25,47 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 		},
 	},
+})
+
+-- Setup LSP keymaps, inlay hints and turn off semantic tokens
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(args)
+		-- Define servers that are allowed to show inlay hints
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local bufnr = args.buf
+		-- Check if the current server is in our allowed list
+		if client == nil then
+			return
+		end
+		client.server_capabilities.semanticTokensProvider = nil
+		if hint_allowed_list[client.name] then
+			if client.server_capabilities.inlayHintProvider then
+				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			end
+		end
+		local opts = { buffer = args.buf, silent = true }
+		opts.desc = "Go to declaration"
+		keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+		opts.desc = "See available code actions"
+		keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+		opts.desc = "Rename symbol"
+		keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+		opts.desc = "Show line diagnostics"
+		keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+		opts.desc = "Go to previous diagnostic"
+		keymap.set("n", "[d", function()
+			vim.diagnostic.jump({ count = -1 })
+		end, opts) -- jump to previous diagnostic in buffer
+		opts.desc = "Go to next diagnostic"
+		keymap.set("n", "]d", function()
+			vim.diagnostic.jump({ count = 1 })
+		end, opts) -- jump to next diagnostic in buffer
+		opts.desc = "Show documentation for what is under cursor"
+		keymap.set("n", "K", function()
+			vim.lsp.buf.hover({ border = "single" })
+		end, opts) -- show documentation for what is under cursor
+		opts.desc = "Restart LSP"
+		keymap.set("n", "<leader>lr", "<cmd>lsp restart<CR>", opts) -- mapping to restart lsp if necessary
+	end,
 })
--- nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -4,35 +4,4 @@ return {
 		"BufReadPre",
 		"BufNewFile",
 	},
-	config = function()
-		local keymap = vim.keymap -- for conciseness
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = { buffer = ev.buf, silent = true }
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-				opts.desc = "Rename symbol"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", function()
-					vim.diagnostic.jump({ count = -1 })
-				end, opts) -- jump to previous diagnostic in buffer
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", function()
-					vim.diagnostic.jump({ count = 1 })
-				end, opts) -- jump to next diagnostic in buffer
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", function()
-					vim.lsp.buf.hover({ border = "single" })
-				end, opts) -- show documentation for what is under cursor
-				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-			end,
-		})
-	end,
 }
