f21cd84f29a36e4f7fe40e205a7e4ca408bafa99
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-12 22:19:24 +0100

    feat: update minuet-ai to remove plenary dependency

--- nvim/.config/nvim/lua/plugins/minuet.lua
+++ nvim/.config/nvim/lua/plugins/minuet.lua
@@ -1,40 +1,37 @@
 return {
-	{
-		"milanglacier/minuet-ai.nvim",
-		lazy = true,
-		keys = {
-			{ "<leader>mt", ":Minuet virtualtext toggle<CR>", desc = "Toggle AI completion" },
-		},
-		config = function()
-			require("minuet").setup({
-				-- Your configuration options here
-				provider = "gemini",
-				provider_options = {
-					gemini = {
-						model = "gemini-2.5-flash-lite",
-						optional = {
-							generationConfig = {
-								maxOutputTokens = 256,
-								thinkingConfig = {
-									thinkingBudget = 0,
-								},
-							},
-						},
-					},
-				},
-				context_window = 4000,
-				context_ratio = 0.75,
-				throttle = 2000,
-				debounce = 800,
-				virtualtext = {
-					auto_trigger_ft = {},
-					keymap = {
-						accept = "<C-y>",
-					},
-				},
-				request_timeout = 2.5,
-			})
-		end,
-	},
-	{ "nvim-lua/plenary.nvim" },
+  "milanglacier/minuet-ai.nvim",
+  lazy = true,
+  keys = {
+    { "<leader>mt", ":Minuet virtualtext toggle<CR>", desc = "Toggle AI completion" },
+  },
+  config = function()
+    require("minuet").setup({
+      -- Your configuration options here
+      provider = "gemini",
+      provider_options = {
+        gemini = {
+          model = "gemini-2.5-flash-lite",
+          optional = {
+            generationConfig = {
+              maxOutputTokens = 256,
+              thinkingConfig = {
+                thinkingBudget = 0,
+              },
+            },
+          },
+        },
+      },
+      context_window = 4000,
+      context_ratio = 0.75,
+      throttle = 2000,
+      debounce = 800,
+      virtualtext = {
+        auto_trigger_ft = {},
+        keymap = {
+          accept = "<C-y>",
+        },
+      },
+      request_timeout = 2.5,
+    })
+  end,
 }
