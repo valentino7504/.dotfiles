a7ca769e9096124a840718330d995c4504e767b8
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-13 14:44:02 +0100

    chore: formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 local ok, secrets = pcall(require, "secrets")
 if ok then
-	vim.env.GEMINI_API_KEY = secrets.GEMINI_API_KEY
+  vim.env.GEMINI_API_KEY = secrets.GEMINI_API_KEY
 end
 require("core")
 require("lazy_setup")
