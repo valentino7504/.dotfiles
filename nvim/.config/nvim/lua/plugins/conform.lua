8d73119e2951bf92bf1dc09edbb2e84353d48e48
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-15 17:06:00 +0100

    chore: formatting fixes

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,36 +1,35 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      formatters_by_ft = {
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        javascript = { "biome" },
-        typescript = { "biome" },
-        javascriptreact = { "biome" },
-        typescriptreact = { "biome" },
-        css = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        html = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        python = { "ruff_organize_imports", "ruff_fix", "ruff_format" },
-        go = { "gofumpt" },
-        zsh = { "beautysh" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-      },
-    })
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = true,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				javascript = { "biome" },
+				typescript = { "biome" },
+				javascriptreact = { "biome" },
+				typescriptreact = { "biome" },
+				css = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				html = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "ruff_organize_imports", "ruff_fix", "ruff_format" },
+				go = { "gofumpt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = true,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
