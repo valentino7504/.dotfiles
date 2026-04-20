53c3f13a02d48b1ccc976c40b257403298bc1cec
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-20 20:50:27 +0100

    feat: rosepine with transparent background

--- nvim/.config/nvim/lua/plugins/rosepine.lua
+++ nvim/.config/nvim/lua/plugins/rosepine.lua
@@ -3,6 +3,11 @@ return {
 	priority = 1000,
 	name = "rose-pine",
 	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparency = true,
+			},
+		})
 		vim.cmd("colorscheme rose-pine-main")
 	end,
 }
