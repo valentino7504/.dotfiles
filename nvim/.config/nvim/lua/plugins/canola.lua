540adaf69da79c3dea7053348a424194f08dc9b1
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-30 01:03:10 +0100

    feat: migrate from oil.nvim to canola.nvim

---
+++ nvim/.config/nvim/lua/plugins/canola.lua
@@ -0,0 +1,45 @@
+return {
+	"barrettruth/canola.nvim",
+	branch = "canola",
+	dependencies = {
+		{
+			"barrettruth/canola-collection",
+			init = function()
+				vim.g.canola_git = {
+					show = { untracked = true, ignored = true },
+					format = "porcelain", -- 'compact' | 'symbol' | 'porcelain'
+				}
+			end,
+		},
+	},
+	keys = {
+		{
+			"-",
+			function()
+				require("canola").toggle_float(vim.fn.getcwd())
+			end,
+			desc = "Open Canola in current working directory",
+		},
+		{
+			"<leader>_",
+			function()
+				require("canola").toggle_float()
+			end,
+			desc = "Open Canola in parent directory of current buffer",
+		},
+	},
+	init = function()
+		vim.g.canola = {
+			highlights = { columns = true },
+			extglob = true,
+			float = { border = "single" },
+			keymaps = {
+				["<BS>"] = { callback = "actions.parent" },
+				["q"] = { callback = "actions.close" },
+				["<Esc>"] = { callback = "actions.close" },
+				["-"] = { callback = "" },
+			},
+			columns = { "git_status", "icon" },
+		}
+	end,
+}
