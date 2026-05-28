1c497dba4eb4cba04d929d161186cba985ba9070
Edwin Ade <edwinmbonyjr@gmail.com>
2026-05-28 22:40:44 +0100

    feat: arrange canola dependencies

--- nvim/.config/nvim/lua/plugins/canola.lua
+++ nvim/.config/nvim/lua/plugins/canola.lua
@@ -1,69 +1,69 @@
 return {
-	"barrettruth/canola.nvim",
-	branch = "canola",
-	dependencies = {
-		{
-			"barrettruth/canola-collection",
-			init = function()
-				vim.g.canola_git = {
-					show = { untracked = true, ignored = true },
-					format = "porcelain", -- 'compact' | 'symbol' | 'porcelain'
-				}
-			end,
-		},
-	},
-	cmd = "Canola",
-	keys = {
-		{
-			"-",
-			function()
-				require("canola").toggle_float(vim.fn.getcwd())
-			end,
-			desc = "Open Canola in current working directory",
-		},
-		{
-			"<leader>_",
-			function()
-				require("canola").toggle_float()
-			end,
-			desc = "Open Canola in parent directory of current buffer",
-		},
-	},
-	init = function(p)
-		if vim.fn.argc() == 1 then
-			local argv = tostring(vim.fn.argv(0))
-			local stat = vim.loop.fs_stat(argv)
-
-			local remote_dir_args = vim.startswith(argv, "ssh")
-				or vim.startswith(argv, "sftp")
-				or vim.startswith(argv, "scp")
-
-			if stat and stat.type == "directory" or remote_dir_args then
-				require("lazy").load({ plugins = { p.name } })
-			end
-		end
-		if not require("lazy.core.config").plugins[p.name]._.loaded then
-			vim.api.nvim_create_autocmd("BufNew", {
-				callback = function()
-					if vim.fn.isdirectory(vim.fn.expand("<afile>")) == 1 then
-						require("lazy").load({ plugins = { "canola.nvim" } })
-						-- Once canola is loaded, we can delete this autocmd
-						return true
-					end
+	{
+		"barrettruth/canola.nvim",
+		branch = "canola",
+		cmd = "Canola",
+		keys = {
+			{
+				"-",
+				function()
+					require("canola").toggle_float(vim.fn.getcwd())
 				end,
-			})
-		end
-		vim.g.canola = {
-			highlights = { columns = true },
-			extglob = true,
-			float = { border = "single" },
-			keymaps = {
-				["<BS>"] = { callback = "actions.parent" },
-				["q"] = { callback = "actions.close" },
-				["<Esc>"] = { callback = "actions.close" },
-				["-"] = { callback = "actions.close" },
+				desc = "Open Canola in current working directory",
 			},
-			columns = { "git_status", "icon" },
-		}
-	end,
+			{
+				"<leader>_",
+				function()
+					require("canola").toggle_float()
+				end,
+				desc = "Open Canola in parent directory of current buffer",
+			},
+		},
+		init = function(p)
+			if vim.fn.argc() == 1 then
+				local argv = tostring(vim.fn.argv(0))
+				local stat = vim.loop.fs_stat(argv)
+
+				local remote_dir_args = vim.startswith(argv, "ssh")
+					or vim.startswith(argv, "sftp")
+					or vim.startswith(argv, "scp")
+
+				if stat and stat.type == "directory" or remote_dir_args then
+					require("lazy").load({ plugins = { p.name } })
+				end
+			end
+			if not require("lazy.core.config").plugins[p.name]._.loaded then
+				vim.api.nvim_create_autocmd("BufNew", {
+					callback = function()
+						if vim.fn.isdirectory(vim.fn.expand("<afile>")) == 1 then
+							require("lazy").load({ plugins = { "canola.nvim" } })
+							-- Once canola is loaded, we can delete this autocmd
+							return true
+						end
+					end,
+				})
+			end
+			vim.g.canola = {
+				highlights = { columns = true },
+				extglob = true,
+				float = { border = "single" },
+				keymaps = {
+					["<BS>"] = { callback = "actions.parent" },
+					["q"] = { callback = "actions.close" },
+					["<Esc>"] = { callback = "actions.close" },
+					["-"] = { callback = "actions.close" },
+				},
+				columns = { "git_status", "icon" },
+			}
+		end,
+	},
+	{
+		"barrettruth/canola-collection",
+		init = function()
+			vim.g.canola_git = {
+				show = { untracked = true, ignored = true },
+				format = "porcelain", -- 'compact' | 'symbol' | 'porcelain'
+			}
+		end,
+	},
 }
