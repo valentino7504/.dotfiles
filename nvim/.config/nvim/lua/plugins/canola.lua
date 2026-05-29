0ca69bb16fb8542ea280dd14178f6f3e9f4a6a7a
Edwin Ade <edwinmbonyjr@gmail.com>
2026-05-29 10:15:48 +0100

    feat: move canola-collection to dependencies

--- nvim/.config/nvim/lua/plugins/canola.lua
+++ nvim/.config/nvim/lua/plugins/canola.lua
@@ -1,46 +1,47 @@
 return {
-	{
-		"barrettruth/canola.nvim",
-		branch = "canola",
-		cmd = "Canola",
-		keys = {
-			{
-				"-",
-				function()
-					require("canola").toggle_float(vim.fn.getcwd())
-				end,
-				desc = "Open Canola in current working directory",
-			},
-			{
-				"<leader>_",
-				function()
-					require("canola").toggle_float()
-				end,
-				desc = "Open Canola in parent directory of current buffer",
-			},
+
+	"barrettruth/canola.nvim",
+	branch = "canola",
+	cmd = "Canola",
+	dependencies = {
+		{
+			"barrettruth/canola-collection",
+			init = function()
+				vim.g.canola_git = {
+					show = { untracked = true, ignored = true },
+					format = "porcelain", -- 'compact' | 'symbol' | 'porcelain'
+				}
+			end,
 		},
-		init = function(p)
-			vim.g.canola = {
-				highlights = { columns = true },
-				extglob = true,
-				float = { border = "single" },
-				keymaps = {
-					["<BS>"] = { callback = "actions.parent" },
-					["q"] = { callback = "actions.close" },
-					["<Esc>"] = { callback = "actions.close" },
-					["-"] = { callback = "actions.close" },
-				},
-				columns = { "git_status", "icon" },
-			}
-		end,
 	},
-	{
-		"barrettruth/canola-collection",
-		init = function()
-			vim.g.canola_git = {
-				show = { untracked = true, ignored = true },
-				format = "porcelain", -- 'compact' | 'symbol' | 'porcelain'
-			}
-		end,
+	keys = {
+		{
+			"-",
+			function()
+				require("canola").toggle_float(vim.fn.getcwd())
+			end,
+			desc = "Open Canola in current working directory",
+		},
+		{
+			"<leader>_",
+			function()
+				require("canola").toggle_float()
+			end,
+			desc = "Open Canola in parent directory of current buffer",
+		},
 	},
+	init = function()
+		vim.g.canola = {
+			highlights = { columns = true },
+			extglob = true,
+			float = { border = "single" },
+			keymaps = {
+				["<BS>"] = { callback = "actions.parent" },
+				["q"] = { callback = "actions.close" },
+				["<Esc>"] = { callback = "actions.close" },
+				["-"] = { callback = "actions.close" },
+			},
+			columns = { "git_status", "icon" },
+		}
+	end,
 }
