733bacbcde08fa8fcdc4fb3b707dd2c8e4d56f46
Edwin Ade <edwinmbonyjr@gmail.com>
2026-05-29 10:15:38 +0100

    feat: add lazy loading to arborist

--- nvim/.config/nvim/lua/plugins/arborist.lua
+++ nvim/.config/nvim/lua/plugins/arborist.lua
@@ -1,10 +1,12 @@
 return {
 	"arborist-ts/arborist.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("arborist").setup({
 			update_cadence = "weekly",
 			ignore = {
 				"canola",
+				"snacks_dashboard",
 			},
 		})
 	end,
